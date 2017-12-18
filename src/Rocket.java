2ef5fe5902c20908a9f89700941c3caed35f7f9d
Alejo Chavez <alejo.chg@gmail.com>
2017-12-18 16:34:57 -0700

    Declaration of class rocket from which U1 and U2 will get most of their fields and methods

--- src/Rocket.java
+++ src/Rocket.java
@@ -1,2 +1,23 @@
-public class Rocket {
+public class Rocket implements SpaceShip{
+    private int weight;
+    private int weightLimit;
+
+    public boolean launch() {
+        return true;
+    }
+
+    public boolean land() {
+        return true;
+    }
+
+    public final boolean canCarry(Item item) {
+        if(item.weight+this.weight > weightLimit) {
+            return false;
+        } else
+            return true;
+    }
+
+    public final void carry(Item item) {
+        this.weight += item.weight;
+    }
 }
