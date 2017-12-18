cc3d0bc44722654efc978278b573dd2921579baa
Alejo Chavez <alejo.chg@gmail.com>
2017-12-18 14:15:48 -0700

    Declaration of interface SpaceShip

--- src/SpaceShip.java
+++ src/SpaceShip.java
@@ -1,2 +1,29 @@
 public interface SpaceShip {
+    /**
+     * Interface that defines the basic methods that a SpaceShip should support
+     */
+
+    /**
+     * Method that returns either true or false indicating if the launch was
+     * successful or if the rocket has crashed.
+     *
+     * @return
+     */
+    boolean launch();
+
+    /**
+     * Method that returns either true or false based on the success of landing
+     */
+    boolean land();
+
+    /**
+     * Method that takes an Item as an argument and returns true if the rocket can
+     * carry such item or false if it will exceed the weight limit.
+     */
+    boolean canCarry(Item item);
+
+    /**
+     * Method that takes an Item object and updates the current weight of the rocket.
+     */
+    void carry(Item item);
 }
