40b1ece81db120e775d06cd0da868e8a205f3c07
Alejo Chavez <alejo.chg@gmail.com>
2017-12-18 17:58:45 -0700

    Declaration of U1 and U2 which are rocket models.

--- src/U1.java
+++ src/U1.java
@@ -1,2 +1,28 @@
-public class U1 {
+public class U1 extends Rocket {
+    public U1() {
+        this.weight = 10000;
+        this.weightLimit = 18000;
+        this.cost = 100000000;
+    }
+
+    @Override
+    public boolean launch() {
+        double chance = 0.05 * (weight/weightLimit);
+        double random = Math.random();
+
+        if(random <= chance)
+            return false;
+        else
+            return true;
+    }
+
+    public boolean land() {
+        double chance = 0.01 * (weight/weightLimit);
+        double random = Math.random();
+
+        if(random <= chance)
+            return false;
+        else
+            return true;
+    }
 }
--- src/U2.java
+++ src/U2.java
@@ -1,2 +1,28 @@
-public class U2 {
+public class U2 extends Rocket {
+    public U2() {
+        this.weight = 18000;
+        this.weightLimit = 29000;
+        this.cost = 120000000;
+    }
+
+    @Override
+    public boolean launch() {
+        double chance = 0.04 * (weight/weightLimit);
+        double random = Math.random();
+
+        if(random <= chance)
+            return false;
+        else
+            return true;
+    }
+
+    public boolean land() {
+        double chance = 0.08 * (weight/weightLimit);
+        double random = Math.random();
+
+        if(random <= chance)
+            return false;
+        else
+            return true;
+    }
 }
